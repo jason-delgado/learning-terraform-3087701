dbc8cfde4ecb5bac7e63c924f54e8a96e4ba63fa
jason-delgado <118306006+jason-delgado@users.noreply.github.com>
2023-07-18 15:29:59 -0500

    create test env

---
+++ test/main.tf
@@ -0,0 +1,11 @@
+module "test" { 
+    source = "../modules/blog"
+
+    environment = {
+        name           = "test"
+        network_prefix = "10.1"
+    }
+
+    asg_min_size = 1
+    asg_max_size = 1
+}
---
+++ test/outputs.tf
@@ -0,0 +1,3 @@
+output "environment_url" {
+    value = module.test.environment_url
+}
