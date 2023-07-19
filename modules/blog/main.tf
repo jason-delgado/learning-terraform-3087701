54c3d0a49dfd47913d6b31e672a65d44bd4ed3f9
jason-delgado <118306006+jason-delgado@users.noreply.github.com>
2023-07-19 16:04:08 -0500

    creating two instances

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -29,21 +29,26 @@ module "blog_vpc" {
   }
 }
 
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.5.2"
+resource "aws_instance" "blog01" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  subnet_id              = module.blog_vpc.public_subnets[0]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  name = "${var.environment.name}-blog"
-  min_size = var.asg_min_size
-  max_size = var.asg_max_size
+  tags = {
+    Name = "Learning Terraform"
+  }
+}
 
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
-  security_groups     = [module.blog_sg.security_group_id]
+resource "aws_instance" "blog02" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  subnet_id              = module.blog_vpc.public_subnets[0]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  image_id       = data.aws_ami.app_ami.id
-  instance_type  = var.instance_type
-  
+  tags = {
+    Name = "Learning Terraform"
+  }
 }
 
 module "blog_alb" {
